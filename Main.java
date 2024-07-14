097db0b1798144cf5102e2204e48258fd2bc718d
Shahad Alhazmi <129505192+Shdd1@users.noreply.github.com>
2024-07-14 17:32:27 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,37 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //*****************Q1**************************************
+        //object
+        /*Account account=new Account();
+        Account account1=new Account("001","Osama");
+        Account account2=new Account("002","shahad",3000);
+
+        System.out.println(account1);
+        System.out.println(account2);
+
+        account1.credit(1000);
+        System.out.println("Balance after crediting to account1: " + account1.getBalance());
+
+        account2.debit(200);
+        System.out.println("Balance after debiting from account2: " + account2.getBalance());
+
+        account2.transferTo(account1, 200);
+        System.out.println("Balance of account1 after transferring from account2: " + account1.getBalance());
+        System.out.println("Balance of account2 after transferring to account1: " + account2.getBalance());
+*/
+
+        //***********************Q2***********************************
+
+        /*Employee employee1 = new Employee("001", "mohanad", 1000);
+        System.out.println(employee1);
+        System.out.println("Employee Annual Salary:"+employee1.getAnnualSalary());
+        System.out.println("Employee New Salary after raise:"+employee1.raisedSalary(20));
+       */
+
+
+    }
+
+
+}
